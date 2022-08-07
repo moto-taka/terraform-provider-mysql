ba4686d3a08546c2b1a077c597995f4ddd8a73c8
Takato Hano <rocky@timetreeapp.com>
2022-08-07 10:29:29 +0900

    update go and mod

--- main.go
+++ main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
+	"github.com/TakatoHano/terraform-provider-mysql/mysql"
 	"github.com/hashicorp/terraform-plugin-sdk/plugin"
-	"github.com/terraform-providers/terraform-provider-mysql/mysql"
 )
 
 func main() {
